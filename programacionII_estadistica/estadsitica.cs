250148a0bd7fad1bf5691588094abaae5fe61a03
luis01 <sarco_luis@hotmail.com>
2020-02-12 13:34:26 -0600

    Creacion de la clase estadistica con su miembros

---
+++ programacionII_estadistica/estadsitica.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programacionII_estadistica
+{
+    class estadsitica
+    {
+        public double suma = 0;
+
+        public double media(String[] serie)
+        {
+            foreach(string valor in serie) {
+                suma += int.Parse(valor);
+            }
+            return suma / serie.Length;
+        }
+    }
+}
