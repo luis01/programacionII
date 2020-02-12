eff6ce83948517251bd4eecf47b2f4abf57a1d3e
luis01 <sarco_luis@hotmail.com>
2020-02-12 13:22:31 -0600

    Diseño de la GUI

---
+++ programacionII_estadistica/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace programacionII_estadistica
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblserie = new System.Windows.Forms.Label();
+            this.txtserie = new System.Windows.Forms.TextBox();
+            this.btnMediaAritmetica = new System.Windows.Forms.Button();
+            this.lblrespuesta_serie = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblserie
+            // 
+            this.lblserie.AutoSize = true;
+            this.lblserie.Location = new System.Drawing.Point(12, 30);
+            this.lblserie.Name = "lblserie";
+            this.lblserie.Size = new System.Drawing.Size(42, 13);
+            this.lblserie.TabIndex = 0;
+            this.lblserie.Text = "SERIE:";
+            // 
+            // txtserie
+            // 
+            this.txtserie.Location = new System.Drawing.Point(60, 27);
+            this.txtserie.Name = "txtserie";
+            this.txtserie.Size = new System.Drawing.Size(184, 20);
+            this.txtserie.TabIndex = 1;
+            // 
+            // btnMediaAritmetica
+            // 
+            this.btnMediaAritmetica.Location = new System.Drawing.Point(22, 109);
+            this.btnMediaAritmetica.Name = "btnMediaAritmetica";
+            this.btnMediaAritmetica.Size = new System.Drawing.Size(79, 32);
+            this.btnMediaAritmetica.TabIndex = 2;
+            this.btnMediaAritmetica.Text = "Calcular X";
+            this.btnMediaAritmetica.UseVisualStyleBackColor = true;
+            // 
+            // lblrespuesta_serie
+            // 
+            this.lblrespuesta_serie.AutoSize = true;
+            this.lblrespuesta_serie.Location = new System.Drawing.Point(19, 75);
+            this.lblrespuesta_serie.Name = "lblrespuesta_serie";
+            this.lblrespuesta_serie.Size = new System.Drawing.Size(14, 13);
+            this.lblrespuesta_serie.TabIndex = 3;
+            this.lblrespuesta_serie.Text = "X";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(368, 201);
+            this.Controls.Add(this.lblrespuesta_serie);
+            this.Controls.Add(this.btnMediaAritmetica);
+            this.Controls.Add(this.txtserie);
+            this.Controls.Add(this.lblserie);
+            this.Name = "Form1";
+            this.Text = "Calculos basicos Estadistica";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblserie;
+        private System.Windows.Forms.TextBox txtserie;
+        private System.Windows.Forms.Button btnMediaAritmetica;
+        private System.Windows.Forms.Label lblrespuesta_serie;
+    }
+}
+
