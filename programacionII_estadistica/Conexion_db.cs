0ff3695f5b5a1e51a47be1971bef5a364777cc32
Luis Hernandez <sarco_luis@hotmail.com>
2020-03-30 21:02:52 -0600

    Terminacion del Registro de Clientes

---
+++ programacionII_estadistica/Conexion_db.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Libreria para trabajar con BD
+using System.Data;
+using System.Data.SqlClient; //Libreria para conectarse una BD de SQL Server
+
+namespace programacionII_estadistica
+{
+    class Conexion_db
+    {
+        SqlConnection miConexion = new SqlConnection();
+        SqlCommand comandosSQL = new SqlCommand();
+        SqlDataAdapter miAdaptadorDatos = new SqlDataAdapter();
+
+        DataSet ds = new DataSet();
+
+        public Conexion_db()
+        {
+            String cadena = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\sistema_db.mdf;Integrated Security=True";
+            miConexion.ConnectionString = cadena;
+            miConexion.Open();
+        }
+        public DataSet obtener_datos()
+        {
+            ds.Clear();
+            comandosSQL.Connection = miConexion;
+            
+            comandosSQL.CommandText = "select * from clientes";
+            miAdaptadorDatos.SelectCommand = comandosSQL;
+            miAdaptadorDatos.Fill(ds, "clientes");
+
+            comandosSQL.CommandText = "select * from productos";
+            miAdaptadorDatos.SelectCommand = comandosSQL;
+            miAdaptadorDatos.Fill(ds, "productos");
+
+            return ds;
+        }
+        public void mantenimiento_datos(String[] datos, String accion)
+        {
+            String sql = "";
+            if (accion == "nuevo") {
+                sql = "INSERT INTO clientes (codigo,nombre,direccion,telefono,dui,nit) VALUES(" +
+                    "'" + datos[1] + "'," +
+                    "'" + datos[2] + "'," +
+                    "'" + datos[3] + "'," +
+                    "'" + datos[4] + "'," +
+                    "'" + datos[5] + "'," +
+                    "'" + datos[6] + "'" +
+                    ")";
+            } else if (accion == "modificar") {
+                sql = "UPDATE clientes SET " +
+                    "codigo         = '" + datos[1] + "'," +
+                    "nombre         = '" + datos[2] + "'," +
+                    "direccion      = '" + datos[3] + "'," +
+                    "telefono       = '" + datos[4] + "'," +
+                    "dui            = '" + datos[5] + "'," +
+                    "nit            = '" + datos[6] + "'" +
+                    "WHERE idCliente = '"+ datos[0] +"'";
+            } else if (accion=="eliminar") {
+                sql = "DELETE clientes FROM clientes WHERE idCliente='"+ datos[0] +"'";
+            }
+            procesarSQL(sql);
+        }
+        void procesarSQL(String sql)
+        {
+            comandosSQL.Connection = miConexion;
+            comandosSQL.CommandText = sql;
+            comandosSQL.ExecuteNonQuery();
+        }
+    }
+}
--- programacionII_estadistica/Form1.Designer.cs
+++ programacionII_estadistica/Form1.Designer.cs
@@ -28,21 +28,350 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.grbDatosClientes = new System.Windows.Forms.GroupBox();
+            this.txtnit = new System.Windows.Forms.TextBox();
+            this.lblnit = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtdui = new System.Windows.Forms.TextBox();
+            this.lbldui = new System.Windows.Forms.Label();
+            this.txttelefono = new System.Windows.Forms.TextBox();
+            this.lbltelefono = new System.Windows.Forms.Label();
+            this.txtdireccion = new System.Windows.Forms.TextBox();
+            this.lbldireccion = new System.Windows.Forms.Label();
+            this.txtnombre = new System.Windows.Forms.TextBox();
+            this.lblnombre = new System.Windows.Forms.Label();
+            this.txtcodigo = new System.Windows.Forms.TextBox();
+            this.lblcodigo = new System.Windows.Forms.Label();
+            this.grbNavegacion = new System.Windows.Forms.GroupBox();
+            this.btnultimo = new System.Windows.Forms.Button();
+            this.btnsiguiente = new System.Windows.Forms.Button();
+            this.btnanterior = new System.Windows.Forms.Button();
+            this.btnprimero = new System.Windows.Forms.Button();
+            this.grbEdicion = new System.Windows.Forms.GroupBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btneliminar = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.lblidCliente = new System.Windows.Forms.Label();
+            this.lblnregistros = new System.Windows.Forms.Label();
+            this.grbDatosClientes.SuspendLayout();
+            this.grbNavegacion.SuspendLayout();
+            this.grbEdicion.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // grbDatosClientes
+            // 
+            this.grbDatosClientes.Controls.Add(this.lblidCliente);
+            this.grbDatosClientes.Controls.Add(this.txtnit);
+            this.grbDatosClientes.Controls.Add(this.lblnit);
+            this.grbDatosClientes.Controls.Add(this.textBox6);
+            this.grbDatosClientes.Controls.Add(this.label6);
+            this.grbDatosClientes.Controls.Add(this.txtdui);
+            this.grbDatosClientes.Controls.Add(this.lbldui);
+            this.grbDatosClientes.Controls.Add(this.txttelefono);
+            this.grbDatosClientes.Controls.Add(this.lbltelefono);
+            this.grbDatosClientes.Controls.Add(this.txtdireccion);
+            this.grbDatosClientes.Controls.Add(this.lbldireccion);
+            this.grbDatosClientes.Controls.Add(this.txtnombre);
+            this.grbDatosClientes.Controls.Add(this.lblnombre);
+            this.grbDatosClientes.Controls.Add(this.txtcodigo);
+            this.grbDatosClientes.Controls.Add(this.lblcodigo);
+            this.grbDatosClientes.Enabled = false;
+            this.grbDatosClientes.Location = new System.Drawing.Point(42, 42);
+            this.grbDatosClientes.Name = "grbDatosClientes";
+            this.grbDatosClientes.Size = new System.Drawing.Size(1225, 790);
+            this.grbDatosClientes.TabIndex = 0;
+            this.grbDatosClientes.TabStop = false;
+            this.grbDatosClientes.Text = "Datos de Cliente";
+            // 
+            // txtnit
+            // 
+            this.txtnit.Location = new System.Drawing.Point(219, 655);
+            this.txtnit.Name = "txtnit";
+            this.txtnit.Size = new System.Drawing.Size(358, 38);
+            this.txtnit.TabIndex = 11;
+            // 
+            // lblnit
+            // 
+            this.lblnit.AutoSize = true;
+            this.lblnit.Location = new System.Drawing.Point(48, 661);
+            this.lblnit.Name = "lblnit";
+            this.lblnit.Size = new System.Drawing.Size(67, 32);
+            this.lblnit.TabIndex = 10;
+            this.lblnit.Text = "NIT:";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(410, 1355);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 38);
+            this.textBox6.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(120, 1355);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(93, 32);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "label6";
+            // 
+            // txtdui
+            // 
+            this.txtdui.Location = new System.Drawing.Point(219, 564);
+            this.txtdui.Name = "txtdui";
+            this.txtdui.Size = new System.Drawing.Size(358, 38);
+            this.txtdui.TabIndex = 7;
+            // 
+            // lbldui
+            // 
+            this.lbldui.AutoSize = true;
+            this.lbldui.Location = new System.Drawing.Point(48, 570);
+            this.lbldui.Name = "lbldui";
+            this.lbldui.Size = new System.Drawing.Size(70, 32);
+            this.lbldui.TabIndex = 6;
+            this.lbldui.Text = "DUI:";
+            // 
+            // txttelefono
+            // 
+            this.txttelefono.Location = new System.Drawing.Point(219, 490);
+            this.txttelefono.Name = "txttelefono";
+            this.txttelefono.Size = new System.Drawing.Size(209, 38);
+            this.txttelefono.TabIndex = 5;
+            // 
+            // lbltelefono
+            // 
+            this.lbltelefono.AutoSize = true;
+            this.lbltelefono.Location = new System.Drawing.Point(35, 490);
+            this.lbltelefono.Name = "lbltelefono";
+            this.lbltelefono.Size = new System.Drawing.Size(75, 32);
+            this.lbltelefono.TabIndex = 4;
+            this.lbltelefono.Text = "TEL:";
+            // 
+            // txtdireccion
+            // 
+            this.txtdireccion.Location = new System.Drawing.Point(219, 213);
+            this.txtdireccion.Multiline = true;
+            this.txtdireccion.Name = "txtdireccion";
+            this.txtdireccion.Size = new System.Drawing.Size(745, 238);
+            this.txtdireccion.TabIndex = 3;
+            // 
+            // lbldireccion
+            // 
+            this.lbldireccion.AutoSize = true;
+            this.lbldireccion.Location = new System.Drawing.Point(35, 245);
+            this.lbldireccion.Name = "lbldireccion";
+            this.lbldireccion.Size = new System.Drawing.Size(178, 32);
+            this.lbldireccion.TabIndex = 2;
+            this.lbldireccion.Text = "DIRECCION:";
+            // 
+            // txtnombre
+            // 
+            this.txtnombre.Location = new System.Drawing.Point(219, 151);
+            this.txtnombre.Name = "txtnombre";
+            this.txtnombre.Size = new System.Drawing.Size(540, 38);
+            this.txtnombre.TabIndex = 3;
+            // 
+            // lblnombre
+            // 
+            this.lblnombre.AutoSize = true;
+            this.lblnombre.Location = new System.Drawing.Point(39, 151);
+            this.lblnombre.Name = "lblnombre";
+            this.lblnombre.Size = new System.Drawing.Size(146, 32);
+            this.lblnombre.TabIndex = 2;
+            this.lblnombre.Text = "NOMBRE:";
+            // 
+            // txtcodigo
+            // 
+            this.txtcodigo.Location = new System.Drawing.Point(219, 84);
+            this.txtcodigo.Name = "txtcodigo";
+            this.txtcodigo.Size = new System.Drawing.Size(196, 38);
+            this.txtcodigo.TabIndex = 1;
+            // 
+            // lblcodigo
+            // 
+            this.lblcodigo.AutoSize = true;
+            this.lblcodigo.Location = new System.Drawing.Point(39, 84);
+            this.lblcodigo.Name = "lblcodigo";
+            this.lblcodigo.Size = new System.Drawing.Size(136, 32);
+            this.lblcodigo.TabIndex = 0;
+            this.lblcodigo.Text = "CODIGO:";
+            // 
+            // grbNavegacion
+            // 
+            this.grbNavegacion.Controls.Add(this.lblnregistros);
+            this.grbNavegacion.Controls.Add(this.btnultimo);
+            this.grbNavegacion.Controls.Add(this.btnsiguiente);
+            this.grbNavegacion.Controls.Add(this.btnanterior);
+            this.grbNavegacion.Controls.Add(this.btnprimero);
+            this.grbNavegacion.Location = new System.Drawing.Point(42, 863);
+            this.grbNavegacion.Name = "grbNavegacion";
+            this.grbNavegacion.Size = new System.Drawing.Size(591, 135);
+            this.grbNavegacion.TabIndex = 1;
+            this.grbNavegacion.TabStop = false;
+            this.grbNavegacion.Text = "Navegacion";
+            // 
+            // btnultimo
+            // 
+            this.btnultimo.Location = new System.Drawing.Point(485, 37);
+            this.btnultimo.Name = "btnultimo";
+            this.btnultimo.Size = new System.Drawing.Size(106, 79);
+            this.btnultimo.TabIndex = 3;
+            this.btnultimo.Text = ">|";
+            this.btnultimo.UseVisualStyleBackColor = true;
+            this.btnultimo.Click += new System.EventHandler(this.btnultimo_Click);
+            // 
+            // btnsiguiente
+            // 
+            this.btnsiguiente.Location = new System.Drawing.Point(381, 37);
+            this.btnsiguiente.Name = "btnsiguiente";
+            this.btnsiguiente.Size = new System.Drawing.Size(106, 79);
+            this.btnsiguiente.TabIndex = 2;
+            this.btnsiguiente.Text = ">";
+            this.btnsiguiente.UseVisualStyleBackColor = true;
+            this.btnsiguiente.Click += new System.EventHandler(this.btnsiguiente_Click);
+            // 
+            // btnanterior
+            // 
+            this.btnanterior.Location = new System.Drawing.Point(110, 37);
+            this.btnanterior.Name = "btnanterior";
+            this.btnanterior.Size = new System.Drawing.Size(106, 79);
+            this.btnanterior.TabIndex = 1;
+            this.btnanterior.Text = "<";
+            this.btnanterior.UseVisualStyleBackColor = true;
+            this.btnanterior.Click += new System.EventHandler(this.btnanterior_Click);
+            // 
+            // btnprimero
+            // 
+            this.btnprimero.Location = new System.Drawing.Point(6, 37);
+            this.btnprimero.Name = "btnprimero";
+            this.btnprimero.Size = new System.Drawing.Size(106, 79);
+            this.btnprimero.TabIndex = 0;
+            this.btnprimero.Text = "|<";
+            this.btnprimero.UseVisualStyleBackColor = true;
+            this.btnprimero.Click += new System.EventHandler(this.btnprimero_Click);
+            // 
+            // grbEdicion
+            // 
+            this.grbEdicion.Controls.Add(this.btnBuscar);
+            this.grbEdicion.Controls.Add(this.btneliminar);
+            this.grbEdicion.Controls.Add(this.btnModificar);
+            this.grbEdicion.Controls.Add(this.btnNuevo);
+            this.grbEdicion.Location = new System.Drawing.Point(639, 863);
+            this.grbEdicion.Name = "grbEdicion";
+            this.grbEdicion.Size = new System.Drawing.Size(628, 135);
+            this.grbEdicion.TabIndex = 2;
+            this.grbEdicion.TabStop = false;
+            this.grbEdicion.Text = "Edicion de Clientes";
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(457, 37);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(165, 79);
+            this.btnBuscar.TabIndex = 4;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // btneliminar
+            // 
+            this.btneliminar.Location = new System.Drawing.Point(290, 37);
+            this.btneliminar.Name = "btneliminar";
+            this.btneliminar.Size = new System.Drawing.Size(165, 79);
+            this.btneliminar.TabIndex = 4;
+            this.btneliminar.Text = "Eliminar";
+            this.btneliminar.UseVisualStyleBackColor = true;
+            this.btneliminar.Click += new System.EventHandler(this.btneliminar_Click);
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Location = new System.Drawing.Point(119, 37);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(165, 79);
+            this.btnModificar.TabIndex = 3;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            this.btnModificar.Click += new System.EventHandler(this.btnModificar_Click);
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.Location = new System.Drawing.Point(6, 37);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(106, 79);
+            this.btnNuevo.TabIndex = 2;
+            this.btnNuevo.Text = "Nuevo";
+            this.btnNuevo.UseVisualStyleBackColor = true;
+            this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click);
+            // 
+            // lblidCliente
+            // 
+            this.lblidCliente.AutoSize = true;
+            this.lblidCliente.Location = new System.Drawing.Point(446, 87);
+            this.lblidCliente.Name = "lblidCliente";
+            this.lblidCliente.Size = new System.Drawing.Size(50, 32);
+            this.lblidCliente.TabIndex = 12;
+            this.lblidCliente.Text = "ID:";
+            // 
+            // lblnregistros
+            // 
+            this.lblnregistros.AutoSize = true;
+            this.lblnregistros.Location = new System.Drawing.Point(222, 61);
+            this.lblnregistros.Name = "lblnregistros";
+            this.lblnregistros.Size = new System.Drawing.Size(0, 32);
+            this.lblnregistros.TabIndex = 13;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1354, 1357);
+            this.ClientSize = new System.Drawing.Size(1301, 1166);
+            this.Controls.Add(this.grbEdicion);
+            this.Controls.Add(this.grbNavegacion);
+            this.Controls.Add(this.grbDatosClientes);
             this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.Name = "Form1";
-            this.Text = "Calculos basicos Estadistica";
+            this.Text = "MANTENIMIENTO CLIENTES";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.grbDatosClientes.ResumeLayout(false);
+            this.grbDatosClientes.PerformLayout();
+            this.grbNavegacion.ResumeLayout(false);
+            this.grbNavegacion.PerformLayout();
+            this.grbEdicion.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox grbDatosClientes;
+        private System.Windows.Forms.GroupBox grbNavegacion;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtdui;
+        private System.Windows.Forms.Label lbldui;
+        private System.Windows.Forms.TextBox txttelefono;
+        private System.Windows.Forms.Label lbltelefono;
+        private System.Windows.Forms.TextBox txtdireccion;
+        private System.Windows.Forms.Label lbldireccion;
+        private System.Windows.Forms.TextBox txtnombre;
+        private System.Windows.Forms.Label lblnombre;
+        private System.Windows.Forms.TextBox txtcodigo;
+        private System.Windows.Forms.Label lblcodigo;
+        private System.Windows.Forms.TextBox txtnit;
+        private System.Windows.Forms.Label lblnit;
+        private System.Windows.Forms.GroupBox grbEdicion;
+        private System.Windows.Forms.Button btnultimo;
+        private System.Windows.Forms.Button btnsiguiente;
+        private System.Windows.Forms.Button btnanterior;
+        private System.Windows.Forms.Button btnprimero;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btneliminar;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Button btnNuevo;
+        private System.Windows.Forms.Label lblidCliente;
+        private System.Windows.Forms.Label lblnregistros;
     }
 }
 
